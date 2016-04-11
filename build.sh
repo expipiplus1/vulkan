f712f0db9f6c4064dfcd90506d7e906b283e818d
David McFarland <corngood@gmail.com>
2016-04-10 23:26:26 -0300

    pass build args

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 pushd generate
-stack build
+stack build $*
 rm -rf out
 cat ./Vulkan-Docs/src/spec/vk.xml | stack exec generate
 popd
 rm -rf src
 mv generate/out src
-stack build
+stack build $*
