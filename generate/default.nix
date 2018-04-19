d165c2dd2d9a303585238212439449dcfaab4cd3
Joe Hermaszewski <git@monoid.al>
2018-04-19 20:29:29 +0800

    simplify generate nix expression

--- generate/default.nix
+++ generate/default.nix
@@ -1,30 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  haskellPackages = pkgs.haskell.packages.ghc822.override{
-    overrides =
-      let overrideAttrs = package: newAttrs: package.override (args: args // {
-              mkDerivation = expr: args.mkDerivation (expr // newAttrs);
-            });
-      in self: super: {
-        };
-      };
-
-  haskellPackageGen = { doFilter ? true }: src:
-    let filteredSrc = builtins.filterSource (n: t: t != "unknown") src;
-        package = pkgs.runCommand "default.nix" {} ''
-          ${pkgs.haskellPackages.cabal2nix}/bin/cabal2nix \
-            ${if doFilter then filteredSrc else src} \
-            > "$out"
-        '';
-    in import package;
-
-  drv = haskellPackages.callPackage (haskellPackageGen {} ./.) {};
+  # Strip out the irrelevant parts of the source
+  src = with pkgs.lib;
+        let p = n: toString ./dist == n;
+        in cleanSourceWith {filter = (n: t: !p n); src = cleanSource ./.;};
 
   extraEnvPackages = with pkgs; [
     asciidoctor
     python3
   ];
+
+  drv = pkgs.haskellPackages.callCabal2nix "generate" src {};
 
   envWithExtras = pkgs.lib.overrideDerivation drv.env (attrs: {
     buildInputs = attrs.buildInputs ++ extraEnvPackages;
