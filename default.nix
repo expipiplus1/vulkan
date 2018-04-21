8ad9fe601b24056d79ba6e7b0acd51dc46a0325f
Joe Hermaszewski <git@monoid.al>
2018-04-21 13:38:24 +0800

    Use newer vector-sized in default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc841" }:
 
 # Strip out the irrelevant parts of the source
 let src = with pkgs.lib;
