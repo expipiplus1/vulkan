4ca5d18d2a558d764bad6de2cb7b3d0350c43d17
Joe Hermaszewski <git@monoid.al>
2018-05-07 18:06:35 +0800

    Better guards

--- generate/src/Spec/Savvy/Platform.hs
+++ generate/src/Spec/Savvy/Platform.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards   #-}
 
 module Spec.Savvy.Platform
@@ -10,8 +11,11 @@ module Spec.Savvy.Platform
 import           Control.Arrow          ((&&&))
 import           Data.Either.Validation
 import qualified Data.Map               as Map
-import           Data.Text
-import qualified Data.Text              as T
+import           Data.Text (Text)
+import qualified Data.Text.Extra              as T
+import           Data.Semigroup ((<>))
+import           Data.Char
+import           Control.Bool
 
 import           Spec.Platform
 import           Spec.Savvy.Error
@@ -35,19 +39,53 @@ getModuleGuardInfo exts platforms =
         Nothing -> Failure [UnknownPlatform p]
         Just g  -> pure g
   in  sequenceA
-        [ PlatformGuardInfo (toModuleName "Extensions" extName) platform
+        [ PlatformGuardInfo (makeModuleName "Extensions" extName) platform
             <$> platformToGuard platform
         | Extension {..} <- exts
         , Just platform  <- [extPlatform]
+        , makeModuleName <- [toModuleName, toCModuleName]
         ]
 
--- | TODO: Reduce duplication
+-- TODO: reduce duplication
+toCModuleName :: Text -> Text -> Text
+toCModuleName feature n = T.intercalate
+  "."
+  [ "Graphics"
+  , "Vulkan"
+  , "C"
+  , sectionNameToModuleBaseName feature
+  , sectionNameToModuleBaseName n
+  ]
+
 toModuleName :: Text -> Text -> Text
 toModuleName feature n = T.intercalate
   "."
   [ "Graphics"
   , "Vulkan"
-  , feature
-  , n
+  , sectionNameToModuleBaseName feature
+  , sectionNameToModuleBaseName n
   ]
 
+sectionNameToModuleBaseName :: Text -> Text
+sectionNameToModuleBaseName = \case
+  "Types not directly used by the API" -> "OtherTypes"
+  "Header boilerplate"                 -> "OtherTypes"
+  t
+    | "Promoted from " `T.isPrefixOf` t
+    -> T.intercalate "_"
+      . fmap (T.replace "+" "and")
+      . T.words
+      . T.takeWhile (/= ',')
+      $ t
+    | Just e <- T.dropPrefix "Originally based on" t
+    -> ("Promoted_From_" <>) . head . T.words $ e
+    | otherwise
+    -> T.concat
+      . fmap T.upperCaseFirst
+      . filter isAllowed
+      . T.words
+      . T.filter ((not . isPunctuation) <||> (== '_'))
+      $ t
+    where
+      isAllowed n = n `notElem` forbiddenWords
+      forbiddenWords = ["commands", "API"]
