a6369a1fbd5c3caa8ef3ebce0887b5045a0ab058
Joe Hermaszewski <git@monoid.al>
2019-04-14 21:56:01 +0800

    Update nix expressions for generator

--- generate/shell.nix
+++ generate/shell.nix
@@ -1,3 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, compiler ? "ghc864"
+, hoogle ? true
+}:
 
-(import ./default.nix { inherit pkgs; }).env
+(import ./default.nix { inherit pkgs compiler hoogle; }).env
