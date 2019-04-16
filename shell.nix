bece0f3abb43a49fa4ce9df0c801b02f7d2d4dc6
Joe Hermaszewski <git@monoid.al>
2019-04-16 10:08:50 +0800

    nix improvements

--- shell.nix
+++ shell.nix
@@ -1,3 +1,6 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc842" }:
+{ pkgs ? import <nixpkgs> {}
+, compiler ? "ghc864"
+, hoogle ? true
+}:
 
-(import ./default.nix { inherit pkgs compiler; }).env
+(import ./default.nix { inherit pkgs compiler hoogle; }).env
