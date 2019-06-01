ffc0bc51a3aa2b615019b65de9b9dcb688d33cc9
Daniel Firth <locallycompact@gmail.com>
2019-06-01 15:03:44 +0100

    Update to lts-13.23 and bump vector-sized

--- default.nix
+++ default.nix
@@ -1,27 +1,17 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc841" }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
 
 # Strip out the irrelevant parts of the source
 let src = with pkgs.lib;
           let p = n: (toString ./generate) == n || (toString ./dist) == n;
           in cleanSourceWith {filter = (n: t: !p n); src = cleanSource ./.;};
 
-    haskellPackages = pkgs.haskell.packages.${compiler}.override {
-      overrides = self: super: {
-        vector-sized = self.vector-sized_1_0_0_0;
-
-        vector-sized_1_0_0_0 = super.vector-sized_1_0_0_0.override {
-          indexed-list-literals = self.indexed-list-literals_0_2_0_0;
-        };
-      };
-    };
-
     extraEnvPackages = [
-      haskellPackages.cabal-install
-      haskellPackages.hscolour
+      pkgs.haskellPackages.cabal-install
+      pkgs.haskellPackages.hscolour
     ];
 
     drv =
-      haskellPackages.callCabal2nix
+      pkgs.haskellPackages.callCabal2nix
         "vulkan"
         src
         { vulkan = pkgs.vulkan-loader; };
