7c0e6f495dd3350da746d3fbcd9e5f6da28bcc8f
Joe Hermaszewski <git@monoid.al>
2020-02-23 19:37:29 +0800

    stub for marshaled commands

--- generate-new/src/Render/Command.hs
+++ generate-new/src/Render/Command.hs
@@ -7,38 +7,51 @@ import           Relude                  hiding ( Reader
                                                 , lift
                                                 )
 import           Data.Text.Prettyprint.Doc
+import           Data.Text.Extra                ( upperCaseFirst )
 import           Language.Haskell.TH.Syntax
 import           Polysemy
 import           Polysemy.Reader
 import qualified Data.Vector                   as V
+
+import           Foreign.Ptr
 
 import           Spec.Parse
 import           Haskell                       as H
 import           Marshal
 import           Marshal.Scheme
 import           Error
-import           CType as C
+import           CType                         as C
 import           Render.Element
 import           Render.Type
 import           Render.Scheme
+import           Render.SpecInfo
+import           Render.Poke
 
 renderCommand
-  :: (HasErr r, Member (Reader RenderParams) r)
+  :: (HasErr r, Member (Reader RenderParams) r, HasSpecInfo r)
   => MarshaledCommand
   -> Sem r RenderElement
 renderCommand m@MarshaledCommand {..} = contextShow mcName $ do
   RenderParams {..} <- ask
+  let Command {..} = mcCommand
   genRe ("command " <> mcName) $ do
-    let n = mkFunName mcName
-    tellExport (ETerm n)
-    nts <- V.mapMaybe id <$> traverseV (paramType schemeType) mcParams
-    r <- makeReturnType m
-    let t = foldr (~>) r nts
-    tString <- renderType t
-    tellDoc
-      $  (pretty n <+> "::" <+> indent 0 tString)
-      <> hardline
-      <> (pretty n <+> "=" <+> "undefined")
+    ffiTy <- cToHsType
+      DoPreserve
+      (Proto cReturnType
+             [ (Nothing, pType) | Parameter {..} <- toList cParameters ]
+      )
+    dynamicBindType <- renderType $ ConT ''FunPtr :@ ffiTy ~> ffiTy
+    let dynName = "mk" <> upperCaseFirst cName
+    marshaledCommandCall dynName m
+    tellDoc $ vsep
+      [ mempty
+      , "foreign import ccall"
+      , "#if !defined(SAFE_FOREIGN_CALLS)"
+      , indent 2 "unsafe"
+      , "#endif"
+      , indent 2 "\"dynamic\"" <+> pretty dynName
+      , indent 2 ("::" <+> dynamicBindType)
+      ]
 
 paramType
   :: (HasErr r, Member (Reader RenderParams) r)
@@ -61,3 +74,29 @@ makeReturnType MarshaledCommand {..} = do
   let ts = pts <> r
   pure $ ConT ''IO :@ foldl' (:@) (TupleT (length ts)) ts
 
+----------------------------------------------------------------
+-- Calling this command
+----------------------------------------------------------------
+
+marshaledCommandCall
+  :: (HasErr r, HasSpecInfo r, HasRenderParams r, HasRenderElem r)
+  => Text
+  -> MarshaledCommand
+  -> Sem r ()
+marshaledCommandCall dynName m@MarshaledCommand {..} = do
+  RenderParams {..} <- ask
+  let n = mkFunName mcName
+  tellExport (ETerm n)
+  nts <- V.mapMaybe id <$> traverseV (paramType schemeType) mcParams
+  r   <- makeReturnType m
+  let t = foldr (~>) r nts
+  tDoc  <- renderType t
+  pokes <- forV mcParams
+    $ \MarshaledParam {..} -> pure $ pretty (pName mpParam) <+> "<- undefined"
+  let call = pretty dynName <+> parens "error \"Fun Ptr\"" <+> sep
+        (pretty . pName . mpParam <$> toList mcParams)
+  tellDoc
+    . vsep
+    $ [ pretty n <+> "::" <+> indent 0 tDoc
+      , pretty n <+> "=" <+> doBlock (toList pokes <> [call])
+      ]
