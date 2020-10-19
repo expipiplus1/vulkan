db4a95aaa24aac1d1b395c345f0f09197b154b15
Joe Hermaszewski <git@monoid.al>
2020-10-19 19:35:47 +0800

    squash warnings in scripts

--- generate-new/flags.sh
+++ generate-new/flags.sh
@@ -3,9 +3,11 @@
 f=$1
 exe_name=$(sed -n 's|^.*generate-new/\([^/]*\)/.*$|\1|p' <<< "$f")
 p=/home/j/projects/vulkan/generate-new/package.yaml
-printf "%s\n" "$(yq < "$p" ".executables.$exe_name.\"source-dirs\"" --raw-output | sed 's|^|-i|')" >> $HIE_BIOS_OUTPUT
-printf "%s\n" "$(yq < "$p" '.library."source-dirs"' --raw-output | sed 's|^|-i|')" >> $HIE_BIOS_OUTPUT
-printf "%s\n" "$(yq < "$p" '.library."ghc-options"[]' --raw-output | sed '/-O./d')" >> $HIE_BIOS_OUTPUT
-printf "%s\n" "$(yq < "$p" '.library."default-extensions"[]' --raw-output | sed 's|^|-X|')" >> $HIE_BIOS_OUTPUT
-printf "%s\n" "$(yq < "$p" '."default-extensions"[]' --raw-output | sed 's|^|-X|')" >> $HIE_BIOS_OUTPUT
-printf "%s\n" "$(yq < "$p" '."ghc-options"[]' --raw-output | sed '/-O/d')" >> $HIE_BIOS_OUTPUT
+{
+  printf "%s\n" "$(yq < "$p" ".executables.$exe_name.\"source-dirs\"" --raw-output | sed 's|^|-i|')"
+  printf "%s\n" "$(yq < "$p" '.library."source-dirs"' --raw-output | sed 's|^|-i|')"
+  printf "%s\n" "$(yq < "$p" '.library."ghc-options"[]' --raw-output | sed '/-O./d')"
+  printf "%s\n" "$(yq < "$p" '.library."default-extensions"[]' --raw-output | sed 's|^|-X|')"
+  printf "%s\n" "$(yq < "$p" '."default-extensions"[]' --raw-output | sed 's|^|-X|')"
+  printf "%s\n" "$(yq < "$p" '."ghc-options"[]' --raw-output | sed '/-O/d')"
+} >> "$HIE_BIOS_OUTPUT"
