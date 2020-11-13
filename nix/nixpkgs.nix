acd660a38836ac27dffefcd37bf5946f5c8ed98c
Joe Hermaszewski <git@monoid.al>
2020-11-13 16:11:30 +0800

    Use nixos nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
     url =
-      "https://github.com/expipiplus1/nixpkgs/archive/8f23b9ae7adc6f2a834589dfb38213e7efc4e654.tar.gz"; # joe-develop-opts
+      "https://github.com/nixos/nixpkgs/archive/d3edc3a53ee646bb225b74d5d99bd7a2df33903d.tar.gz"; # haskell-updates
     sha256 = "188y95a66g0i53ls74fqfwpcfvvkvqz7b7bwfisr5fw3y6s1jjd5";
   };
 
