15199a89a3a4298b8e91519311d93600e4a6c6d6
Joe Hermaszewski <git@monoid.al>
2020-11-23 17:07:05 +0800

    regenerate with SPIR-V info

--- src/Vulkan.hs
+++ src/Vulkan.hs
@@ -5,6 +5,7 @@ module Vulkan  ( module Vulkan.CStruct
                , module Vulkan.Core12
                , module Vulkan.Extensions
                , module Vulkan.NamedType
+               , module Vulkan.SPIRVInfo
                , module Vulkan.Version
                , module Vulkan.Zero
                ) where
@@ -14,6 +15,7 @@ import Vulkan.Core11
 import Vulkan.Core12
 import Vulkan.Extensions
 import Vulkan.NamedType
+import Vulkan.SPIRVInfo
 import Vulkan.Version
 import Vulkan.Zero
 
