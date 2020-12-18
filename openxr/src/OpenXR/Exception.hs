4bb5da7b465a413a5e3efe72ce2fc24009c53dab
Joe Hermaszewski <git@monoid.al>
2020-12-18 13:52:48 +0800

    Expose resultString in openxr

--- openxr/src/OpenXR/Exception.hs
+++ openxr/src/OpenXR/Exception.hs
@@ -1,19 +1,21 @@
 {-# language CPP #-}
 -- No documentation found for Chapter "Exception"
-module OpenXR.Exception  (OpenXrException(..)) where
+module OpenXR.Exception  ( OpenXrException(..)
+                         , resultString
+                         ) where
 
 import GHC.Exception.Type (Exception(..))
 import OpenXR.Core10.Enums.Result (Result)
 import OpenXR.Core10.Enums.Result (Result(..))
--- | This exception is thrown from calls to marshalled Vulkan commands
--- which return a negative VkResult.
-newtype OpenXrException = OpenXrException { vulkanExceptionResult :: Result }
+-- | This exception is thrown from calls to marshalled OpenXR commands
+-- which return a negative 'Result'.
+newtype OpenXrException = OpenXrException { openxrExceptionResult :: Result }
   deriving (Eq, Ord, Read, Show)
 
 instance Exception OpenXrException where
   displayException (OpenXrException r) = show r ++ ": " ++ resultString r
 
--- | A human understandable message for each VkResult
+-- | A human understandable message for each 'Result'
 resultString :: Result -> String
 resultString = \case
   
