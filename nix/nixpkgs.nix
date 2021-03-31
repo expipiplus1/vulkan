f97cb308cfc106d76bb1eb118547300bdf80e85b
Joe Hermaszewski <git@monoid.al>
2021-03-31 22:26:15 +0800

    bump nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
     url =
-      "https://github.com/nixos/nixpkgs/archive/64b4617883844efe0cc20163e007ee636462eb18.tar.gz"; # refs/heads/nixos-unstable
-    sha256 = "1vqqllxzdvvarwydv6yx0qwwl9shqla08ijabvmydi1kwc6388ww";
+      "https://github.com/nixos/nixpkgs/archive/04a2b269d8921505a2969fc9ec25c1f517f2b307.tar.gz"; # refs/heads/nixos-unstable
+    sha256 = "15hgx2i71pqgvzv56jwzfs8rkhjbm35wk1i6mxrqbq6wd0y10isv";
   };
 
 in import nixpkgsSrc { }
