eb0bb45e8e706d5153bb86e3464b09f1c4d59e3a
Joe Hermaszewski <git@monoid.al>
2021-06-21 22:34:01 +0800

    Add no-doc diff to PR text

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,11 +5,47 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+no_doc_diff=
+requested_version=
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  -h | --help) help=1 ;;
+  --no-doc-diff)
+    no_doc_diff="$2"
+    shift
+    ;;
+  --version)
+    requested_version="$2"
+    shift
+    ;;
+  *)
+    echo "Unknown parameter passed: $1" >&2
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+if [ $help ]; then
+  me=$(basename "$0")
+  cat <<EOF
+$me: Update Vulkan-Docs and regenerate source
+
+Options:
+  --version v1.2.145 : Update to this version of Vulkan-Docs rather than the
+                       latest
+  --no-doc-diff file.patch : Generate a diff between the existing and new
+                             version without documentaiton comments
+EOF
+  exit
+fi
+
 # like v1.4.145
 latest_version=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/KhronosGroup/Vulkan-Docs/tags |
   jq --raw-output 'map(.name) | .[]' |
   sort --version-sort | tail -n1)
-version=${1:-$latest_version}
+version=${requested_version:-$latest_version}
 
 # For GitHub actions
 echo "::set-output name=vulkan_version::$version"
@@ -19,9 +55,21 @@ if git -C generate-new/Vulkan-Docs describe --tags | grep "$version"; then
   exit 0
 fi
 
+before=
+if [ "$no_doc_diff" ]; then
+  before=$(nix-build --no-out-link nix/generate.nix --arg withDocs false)
+fi
+
 echo "Updating Vulkan-Docs"
 git -C generate-new/Vulkan-Docs fetch
 git -C generate-new/Vulkan-Docs checkout "$version"
+
+after=
+if [ "$no_doc_diff" ]; then
+  after=$(nix-build --no-out-link nix/generate.nix --arg withDocs false)
+  git diff --no-index "$before"/ "$after"/ > "$no_doc_diff"
+fi
+
 
 git add generate-new/Vulkan-Docs
 
