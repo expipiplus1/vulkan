ddfee41da639bac3907d60bc26151b1f79ef9539
IC Rainbow <aenor.realm@gmail.com>
2026-07-01 16:55:25 +0300

    Accessors cleanup

--- utils/src/Vulkan/Utils/PipelineLayout.hs
+++ utils/src/Vulkan/Utils/PipelineLayout.hs
@@ -18,8 +18,7 @@ module Vulkan.Utils.PipelineLayout
 
 import Control.Monad (foldM)
 import Data.Bits ((.|.))
-import Data.Foldable (toList)
-import Data.List (foldl')
+import Data.Foldable (foldl')
 import qualified Data.Map.Strict as Map
 import Data.Word (Word32)
 import qualified Vulkan.Core10 as Vk
@@ -51,24 +50,24 @@ mergeDescriptorSetLayoutBindings
   => f Vk.DescriptorSetLayoutBinding
   -> Either DescriptorBindingConflict [Vk.DescriptorSetLayoutBinding]
 mergeDescriptorSetLayoutBindings bindings =
-  fmap (fmap snd . Map.toAscList) (foldM step Map.empty (toList bindings))
+  fmap (fmap snd . Map.toAscList) (foldM step Map.empty bindings)
   where
-    step acc b = case Map.lookup (bindingNumber b) acc of
-      Nothing -> Right (Map.insert (bindingNumber b) b acc)
-      Just b0 -> (\b' -> Map.insert (bindingNumber b) b' acc) <$> combine b0 b
+    step acc b = case Map.lookup b.binding acc of
+      Nothing -> Right (Map.insert b.binding b acc)
+      Just b0 -> (\b' -> Map.insert b.binding b' acc) <$> combine b0 b
 
     combine b0 b1
-      | t0 /= t1 = Left (DescriptorBindingConflict (bindingNumber b0) (t0, t1))
+      | t0 /= t1 = Left (DescriptorBindingConflict b0.binding (t0, t1))
       | otherwise =
           Right
             ( b0
-                { Vk.stageFlags = bindingStages b0 .|. bindingStages b1
-                , Vk.descriptorCount = max (bindingCount b0) (bindingCount b1)
+                { Vk.stageFlags = b0.stageFlags .|. b1.stageFlags
+                , Vk.descriptorCount = max b0.descriptorCount b1.descriptorCount
                 }
             )
       where
-        t0 = bindingType b0
-        t1 = bindingType b1
+        t0 = b0.descriptorType
+        t1 = b1.descriptorType
 
 {- | Merge the push-constant ranges contributed by several stages: ranges sharing
 the same @(offset, size)@ have their 'Vk.stageFlags' OR-ed. The result is
@@ -81,28 +80,5 @@ mergePushConstantRanges ranges =
   | ((off, sz), stage) <- Map.toAscList byRange
   ]
   where
-    byRange = foldl' add Map.empty (toList ranges)
-    add m r = Map.insertWith (.|.) (rangeOffset r, rangeSize r) (rangeStages r) m
-
--- Field accessors (the constructor disambiguates DuplicateRecordFields).
-
-bindingNumber :: Vk.DescriptorSetLayoutBinding -> Word32
-bindingNumber Vk.DescriptorSetLayoutBinding{Vk.binding = b} = b
-
-bindingType :: Vk.DescriptorSetLayoutBinding -> Vk.DescriptorType
-bindingType Vk.DescriptorSetLayoutBinding{Vk.descriptorType = t} = t
-
-bindingStages :: Vk.DescriptorSetLayoutBinding -> Vk.ShaderStageFlags
-bindingStages Vk.DescriptorSetLayoutBinding{Vk.stageFlags = s} = s
-
-bindingCount :: Vk.DescriptorSetLayoutBinding -> Word32
-bindingCount Vk.DescriptorSetLayoutBinding{Vk.descriptorCount = c} = c
-
-rangeOffset :: Vk.PushConstantRange -> Word32
-rangeOffset Vk.PushConstantRange{Vk.offset = o} = o
-
-rangeSize :: Vk.PushConstantRange -> Word32
-rangeSize Vk.PushConstantRange{Vk.size = s} = s
-
-rangeStages :: Vk.PushConstantRange -> Vk.ShaderStageFlags
-rangeStages Vk.PushConstantRange{Vk.stageFlags = s} = s
+    byRange = foldl' add Map.empty ranges
+    add m r = Map.insertWith (.|.) (r.offset, r.size) r.stageFlags m
